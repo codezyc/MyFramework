4dc70c00bce29f943360b343cf11157588a4d876
codezyc <zyc199144@gmail.com>
2013-11-26 00:04:25 +0800

    修改

--- Framework/CommonLibary/DateUtils.cs
+++ Framework/CommonLibary/DateUtils.cs
@@ -9,7 +9,97 @@ namespace CommonLibary
     /// <summary>
     /// 日期常用操作
     /// </summary>
-    public class DateUtils
+    public static class DateUtils
     {
+        /// <summary>
+        /// 获取两个日期的时间差
+        /// </summary>
+        /// <param name="d1"></param>
+        /// <param name="d2"></param>
+        public static void GetDateDiff(DateTime d1, DateTime d2)
+        {
+            TimeSpan ts = d2.Subtract(d1);
+            Console.WriteLine(ts.TotalDays);
+            Console.WriteLine(ts.TotalHours);
+            Console.WriteLine(ts.TotalMinutes);
+            Console.WriteLine(ts.TotalSeconds);
+        }
+
+        /// <summary>
+        /// 获得今天是星期几
+        /// </summary>
+        /// <returns></returns>
+        public static string GetDayOfWeek()
+        {
+            return DateTime.Now.DayOfWeek.ToString();
+        }
+
+        /// <summary>
+        /// 获得当月的天数
+        /// </summary>
+        /// <returns></returns>
+        public static int GetDaysOfThisMonth()
+        {
+            return DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month);
+        }
+
+        /// <summary>
+        /// 判断今年是否是闰年
+        /// </summary>
+        /// <returns></returns>
+        public static bool GetDaysOfThisYear()
+        {
+            return DateTime.IsLeapYear(DateTime.Now.Year);
+        }
+
+        /// <summary>
+        /// 比较两个日期的大小
+        /// </summary>
+        /// <param name="d1"></param>
+        /// <param name="d2"></param>
+        /// <returns>返回结果：-1：d1 < d2；0：d1 = d2；1：d1 > d2</returns>
+        public static int CompareTwoDate(DateTime d1, DateTime d2)
+        {
+            return d1.CompareTo(d2);
+        }
+
+        /// <summary>
+        /// 昨天
+        /// </summary>
+        /// <returns></returns>
+        public static DateTime GetYesterday()
+        {
+            return DateTime.Today.AddDays(-1);
+        }
+
+        /// <summary>
+        /// 明天
+        /// </summary>
+        /// <returns></returns>
+        public static DateTime GetTomorrow()
+        {
+            return DateTime.Today.AddDays(1);
+        }
+
+        /// <summary>
+        /// 获得本年度的第一天
+        /// </summary>
+        /// <param name="d1"></param>
+        /// <returns></returns>
+        public static DateTime GetFirstDayOfYear(DateTime d1)
+        {
+            return new DateTime(d1.Year, 1, 1);
+        }
+
+        /// <summary>
+        /// 获得本年度的最后一天
+        /// </summary>
+        /// <param name="d1"></param>
+        /// <returns></returns>
+        public static DateTime GetLastDayOfYear(DateTime d1)
+        {
+            DateTime d2 = new DateTime(d1.Year + 1, 1, 1);
+            return d2.AddDays(-1);
+        }
     }
 }
--- Framework/CommonLibary/StringUtils.cs
+++ Framework/CommonLibary/StringUtils.cs
@@ -9,8 +9,50 @@ namespace CommonLibary
     /// <summary>
     /// 字符串的常用操作
     /// </summary>
-    public class StringUtils
+    public static class StringUtils
     {
+        /// <summary>
+        /// 分隔字符串
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="chars"></param>
+        /// <returns></returns>
+        public static string[] SplitByMutipleChar(string s,char[] chars)
+        {
+            return s.Split(chars, StringSplitOptions.RemoveEmptyEntries);
+        }
 
+        /// <summary>
+        /// 获取字符数组
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static char[] GetCharArray(string s)
+        {
+            return s.ToCharArray();
+        }
+
+        /// <summary>
+        /// 字符串逆序
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string ReverseString(string s)
+        {
+            char[] c = s.ToCharArray();
+            Array.Reverse(c);
+            return c.ToString();
+        }
+
+        /// <summary>
+        /// 扩展string.format方法
+        /// </summary>
+        /// <param name="format"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static string FormatWith(this string format,params object[] args)
+        {
+            return string.Format(format, args);
+        }
     }
 }
