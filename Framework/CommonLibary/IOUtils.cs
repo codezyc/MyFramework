ab28535319bed80c90749d53424ccb5c88a6181d
codezyc <zyc199144@gmail.com>
2013-11-30 13:20:49 +0800

    添加IO常用操作方法

--- Framework/CommonLibary/IOUtils.cs
+++ Framework/CommonLibary/IOUtils.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Diagnostics;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualBasic.Devices;
+using System.Windows.Forms;
+using System.Management;
 
 namespace CommonLibary
 {
@@ -212,7 +215,7 @@ namespace CommonLibary
             if (File.Exists(path))
             {
                 FileInfo fileinfo = new FileInfo(path);
-                if(!fileinfo.IsReadOnly)
+                if (!fileinfo.IsReadOnly)
                     fileinfo.Attributes = FileAttributes.ReadOnly;
             }
         }
@@ -231,6 +234,62 @@ namespace CommonLibary
             }
         }
 
+        /// <summary>
+        /// 删除指定路径的文件或文件夹
+        /// </summary>
+        /// <param name="path">文件或文件夹路径</param>
+        public static void DeleteFile(string path)
+        {
+            if (File.Exists(path))
+                File.Delete(path);
+        }
+
+        /// <summary>
+        /// 压缩文件
+        /// </summary>
+        /// <param name="filepath">待压缩文件路径</param>
+        /// <param name="zipfilepath">压缩后文件路径</param>
+        public static void ZipFile(string filepath, string zipfilepath)
+        {
+            if (File.Exists(filepath))
+            {
+                FileInfo fileinfo = new FileInfo(filepath);
+                using (FileStream fs = fileinfo.OpenRead())
+                {
+                    if (File.Exists(zipfilepath))
+                        DeleteFile(zipfilepath);
+                    using (FileStream compressedstream = File.Create(zipfilepath))
+                    {
+                        using (GZipStream zipstream = new GZipStream(compressedstream, CompressionMode.Compress))
+                        {
+                            fs.CopyTo(zipstream);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 解压缩文件
+        /// </summary>
+        /// <param name="zipfilepath">压缩文件路径</param>
+        /// <param name="filepath">解压后文件路径</param>
+        public static void UnZipFile(string zipfilepath, string filepath)
+        {
+            FileInfo fileinfo = new FileInfo(zipfilepath);
+            using (FileStream fs = fileinfo.OpenRead())
+            {
+                if (File.Exists(zipfilepath))
+                    DeleteFile(zipfilepath);
+                using (FileStream decompress = File.Create(filepath))
+                {
+                    using (GZipStream gzip = new GZipStream(fs, CompressionMode.Decompress))
+                    {
+                        gzip.CopyTo(decompress);
+                    }
+                }
+            }
+        }
 
         #endregion
 
@@ -263,17 +322,63 @@ namespace CommonLibary
         /// </summary>
         /// <param name="path">文件夹路径</param>
         /// <param name="newdirectoryname"></param>
-        public static void ReNameDirectory(string path,string newdirectoryname)
+        public static void ReNameDirectory(string path, string newdirectoryname)
         {
             Computer cp = new Computer();
             cp.FileSystem.RenameDirectory(path, newdirectoryname);
         }
 
+        /// <summary>
+        /// 递归删除文件夹
+        /// </summary>
+        /// <param name="path">待删除文件夹的路径</param>
+        public static void RecursionDeleteDirectories(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                foreach (var dir in Directory.GetDirectories(path))
+                {
+                    if (File.Exists(dir))
+                    {
+                        File.Delete(dir);
+                    }
+                    else
+                        RecursionDeleteDirectories(dir);
+                }
+                Directory.Delete(path);
+            }
+        }
+
+        /// <summary>
+        /// 递归复制文件夹
+        /// </summary>
+        /// <param name="currentpath">文件夹路径</param>
+        /// <param name="copypath">目标路径</param>
+        public static void RecursionCopyDirectory(string currentpath, string copypath)
+        {
+            currentpath = currentpath.EndsWith(@"\") ? currentpath : currentpath + @"\";
+            copypath = copypath.EndsWith(@"\") ? copypath : copypath + @"\";
+            if (Directory.Exists(currentpath))
+            {
+                if (Directory.Exists(copypath))
+                {
+                    Directory.CreateDirectory(copypath);
+                }
+                foreach (var file in Directory.GetFiles(currentpath))
+                {
+                    File.Copy(currentpath, copypath);
+                }
+                foreach (var dir in Directory.GetDirectories(currentpath))
+                {
+                    DirectoryInfo dirinfo = new DirectoryInfo(dir);
+                    RecursionCopyDirectory(currentpath + dirinfo.Name, copypath + dirinfo.Name);
+                }
+            }
+        }
 
         #endregion
 
-
-        #region 驱动器相关
+        #region 硬件相关
         /// <summary>
         /// 获取驱动器信息
         /// </summary>
@@ -287,6 +392,82 @@ namespace CommonLibary
                 drivers.Add(driveInfo);
             }
             return drivers;
+        }
+
+        /// <summary>
+        /// 获取机器CPU相关信息
+        /// </summary>
+        /// <returns></returns>
+        public static string GetCPUInfo()
+        {
+            string cpuinfo = string.Empty;
+            ManagementClass mc = new ManagementClass("Win32_Processor");
+            ManagementObjectCollection moc = mc.GetInstances();
+            foreach (var item in moc)
+            {
+                cpuinfo = cpuinfo + item.Properties["ProccessorId"].Value.ToString();
+            }
+            return cpuinfo;
+        }
+
+        /// <summary>
+        /// 获取硬盘的序列号
+        /// </summary>
+        /// <returns></returns>
+        public static string GetHardDriveNumber()
+        {
+            string number = string.Empty;
+            ManagementClass mc = new ManagementClass("Win32_DiskDrive");
+            ManagementObjectCollection moc = mc.GetInstances();
+            foreach (var item in moc)
+            {
+                number = number + item.Properties["Model"].Value.ToString();
+            }
+            return number;
+        }
+
+        /// <summary>
+        /// 获取网卡地址
+        /// </summary>
+        /// <returns></returns>
+        public static string GetMacAddress()
+        {
+            string macaddress = string.Empty;
+            ManagementClass mc = new ManagementClass("Win32_NetWorkAdapterConfiguration");
+            ManagementObjectCollection moc = mc.GetInstances();
+            foreach (var item in moc)
+            {
+                if ((bool)item["IPEnabled"] == true)
+                    macaddress = macaddress + item.Properties["MacAddress"].ToString();
+            }
+            return macaddress;
+        }
+
+        #endregion
+
+        #region 剪贴板相关
+        /// <summary>
+        /// 向剪贴板上设置数据
+        /// </summary>
+        /// <param name="content">设置内容</param>
+        /// <param name="IsKeep">程序退出时剪贴板是否保持设置的内容</param>
+        public static void SetDataOnClipBoard(string content, bool IsKeep)
+        {
+            Clipboard.SetDataObject(content, IsKeep);
+        }
+
+        /// <summary>
+        /// 从剪贴板获取数据
+        /// </summary>
+        /// <returns></returns>
+        public static string GetDataFromClipBoard()
+        {
+            IDataObject dataobj = Clipboard.GetDataObject();
+            if (dataobj.GetDataPresent(DataFormats.Text))
+            {
+                return dataobj.GetData(DataFormats.Text).ToString();
+            }
+            return string.Empty;
         }
 
         #endregion
