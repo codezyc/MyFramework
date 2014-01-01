3cc882fde23eced5f0d287750245c43ee7fadffa
codezyc <zyc199144@gmail.com>
2014-01-01 14:08:34 +0800

    签入

---
+++ Framework/Service/Interface/ILoggingRepository.cs
@@ -0,0 +1,14 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Interface
+{
+    public interface ILoggingRepository
+    {
+        void Log(ApiLoggingInfo loggingInfo);
+    }
+}
