497fa61bd1c023690e73ba64fb6dd3e5612ba566
codezyc <zyc199144@gmail.com>
2014-03-13 12:16:07 +0800

    提交代码

---
+++ Framework/CommonLibary/DateTimeUtils.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibary
+{
+    /// <summary>
+    /// 日期常用操作
+    /// </summary>
+    public static class DateTimeUtils
+    {
+        /// <summary>
+        /// 获取两个日期的时间差
+        /// </summary>
+        /// <param name="d1"></param>
+        /// <param name="d2"></param>
+        public static TimeSpan GetDateDiff(DateTime d1, DateTime d2)
+        {
+            TimeSpan ts = d2.Subtract(d1);
+            return ts;
+        }
+
+        /// <summary>
+        /// 获得今天是星期几
+        /// </summary>
+        /// <returns></returns>
+        public static string GetDayOfWeek()
+        {
+            return DateTime.Now.DayOfWeek.ToString();
+        }
+
+        /// <summary>
+        /// 获得当月的天数
+        /// </summary>
+        /// <returns></returns>
+        public static int GetDaysOfThisMonth()
+        {
+            return DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month);
+        }
+
+        /// <summary>
+        /// 判断今年是否是闰年
+        /// </summary>
+        /// <returns></returns>
+        public static bool GetDaysOfThisYear()
+        {
+            return DateTime.IsLeapYear(DateTime.Now.Year);
+        }
+
+        /// <summary>
+        /// 比较两个日期的大小
+        /// </summary>
+        /// <param name="d1"></param>
+        /// <param name="d2"></param>
+        /// <returns>返回结果：-1:d1< d2, 0:d1 = d2;1:d1 > d2</returns>
+        public static int CompareTwoDate(DateTime d1, DateTime d2)
+        {
+            return d1.CompareTo(d2);
+        }
+
+        /// <summary>
+        /// 昨天
+        /// </summary>
+        /// <returns></returns>
+        public static DateTime GetYesterday()
+        {
+            return DateTime.Today.AddDays(-1);
+        }
+
+        /// <summary>
+        /// 明天
+        /// </summary>
+        /// <returns></returns>
+        public static DateTime GetTomorrow()
+        {
+            return DateTime.Today.AddDays(1);
+        }
+
+        /// <summary>
+        /// 获得本年度的第一天
+        /// </summary>
+        /// <param name="d1"></param>
+        /// <returns></returns>
+        public static DateTime GetFirstDayOfYear(DateTime d1)
+        {
+            return new DateTime(d1.Year, 1, 1);
+        }
+
+        /// <summary>
+        /// 获得本年度的最后一天
+        /// </summary>
+        /// <param name="d1"></param>
+        /// <returns></returns>
+        public static DateTime GetLastDayOfYear(DateTime d1)
+        {
+            DateTime d2 = new DateTime(d1.Year + 1, 1, 1);
+            return d2.AddDays(-1);
+        }
+
+        /// <summary>
+        /// 获取时间戳
+        /// </summary>
+        /// <returns></returns>
+        public static int GetTimeStamp()
+        {
+            TimeSpan ts = DateTime.Now - new DateTime(1970, 1, 1, 0, 0, 0);
+            return Convert.ToInt32(ts.TotalSeconds);
+        }
+
+        /// <summary>
+        /// 获取时间戳重载方法
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static int GetTimeStamp(DateTime time)
+        {
+            TimeSpan ts = time - new DateTime(1970, 1, 1, 0, 0, 0);
+            return Convert.ToInt32(ts.TotalSeconds);
+        }
+    }
+}
--- Framework/MVCExtend/CustomeOutputProvider/DocumentDatabaseOutputCacheProvider.cs
+++ Framework/MVCExtend/CustomeOutputProvider/DocumentDatabaseOutputCacheProvider.cs
@@ -1,77 +1,89 @@
 ﻿using Model;
 using MongoDB.Driver;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Security.Cryptography;
 using System.Text;
-using System.Web;
 using System.Web.Caching;
+using MongoDB.Driver.Builders;
 
 namespace MVCExtend.CustomeOutputProvider
 {
     public class DocumentDatabaseOutputCacheProvider : OutputCacheProvider, IDisposable
     {
-        readonly MongoDB.Driver.MongoServer _mongo;
-        readonly MongoDB.Driver.MongoCollection<CacheItem> _cacheItems;
+        private MongoClient _mongoclient;
+        private MongoServer _mongoserver;
+        private MongoCollection<CacheItem> _cacheItems;
 
         public DocumentDatabaseOutputCacheProvider()
         {
-            //_mongo = new MongoServer(new MongoServerSettings());
-            //_mongo.Connect();
 
-            //var store = _mongo.GetDatabase("OutputCacheDB");
-            //_cacheItems = store.GetCollection()
+            var connectionString = "mongodb://localhost";
+            _mongoclient = new MongoClient(connectionString);
+            _mongoserver = _mongoclient.GetServer();
+
+            var database = _mongoserver.GetDatabase("");
+            _cacheItems = database.GetCollection<CacheItem>("");
         }
 
+        /// <summary>
+        /// 重写OutputCacheProvider的Get方法
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public override object Get(string key)
         {
             Debug.WriteLine(string.Format("Cache.Get({0})", key));
-
             key = MD5(key);
+            var query = Query<CacheItem>.EQ(e => e.Id, key);
+            var cacheItem = _cacheItems.FindOne(query);
 
-            //var cacheItem = _cacheItems.FindOne(new { _id = key });      //FindOne方法有问题
-
-            //if (cacheItem != null)
-            //{
-            //    if (cacheItem.Expiration.ToUniversalTime() <= DateTime.UtcNow)
-            //    {
-            //        _cacheItems.Remove(cacheItem);
-            //    }
-            //    else
-            //    {
-            //        return Deserialize(cacheItem.Item);
-            //    }
-            //}
-
+            if (cacheItem != null)
+            {
+                if (cacheItem.Expiration.ToUniversalTime() <= DateTime.UtcNow)
+                {
+                    _cacheItems.Remove(query);
+                }
+                else
+                {
+                    return Deserialize(cacheItem.Item);
+                }
+            }
             return null;
         }
 
+        /// <summary>
+        /// 重写OutputCacheProvider的Add方法
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="entry"></param>
+        /// <param name="utcExpiry"></param>
+        /// <returns></returns>
         public override object Add(string key, object entry, DateTime utcExpiry)
         {
             Debug.WriteLine("Cache.Add({0}, {1}, {2})", key, entry, utcExpiry);
 
             key = MD5(key);
 
-            //if (utcExpiry == DateTime.MaxValue)
-            //    utcExpiry = DateTime.UtcNow.AddMinutes(5);
+            if (utcExpiry == DateTime.MaxValue)
+                utcExpiry = DateTime.UtcNow.AddMinutes(5);
 
-            //var item = _cacheItems.FindOne(new { _id = key });       //FindOne方法有问题
+            var query = Query<CacheItem>.EQ(e => e.Id, key);
+            var item = _cacheItems.FindOne(query);
 
-            //if (item != null)
-            //{
-            //    if (item.Expiration.ToUniversalTime() <= DateTime.UtcNow)
-            //    {
-            //        _cacheItems.Remove(item);
-            //    }
-            //    else
-            //    {
-            //        return Deserialize(item.Item);
-            //    }
-            //}
+            if (item != null)
+            {
+                if (item.Expiration.ToUniversalTime() <= DateTime.UtcNow)
+                {
+                    _cacheItems.Remove(query);
+                }
+                else
+                {
+                    return Deserialize(item.Item);
+                }
+            }
 
             _cacheItems.Insert(new CacheItem
             {
@@ -83,38 +95,49 @@ namespace MVCExtend.CustomeOutputProvider
             return entry;
         }
 
+        /// <summary>
+        /// 重写OutputCacheProvider的Set方法
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="entry"></param>
+        /// <param name="utcExpiry"></param>
         public override void Set(string key, object entry, DateTime utcExpiry)
         {
             Debug.WriteLine("Cache.Set({0}, {1}, {2})", key, entry, utcExpiry);
 
             key = MD5(key);
 
-            //var item = _cacheItems.FindOne(new { _id = key });   //FindOne方法有问题
+            var query = Query<CacheItem>.EQ(e => e.Id, key);
+            var item = _cacheItems.FindOne(query);
 
-            //if (item != null)
-            //{
-            //    item.Item = Serialize(entry);
-            //    item.Expiration = utcExpiry;
-            //    _cacheItems.Save(item);
-            //}
-            //else
-            //{
-            //    _cacheItems.Insert(new CacheItem
-            //    {
-            //        Id = key,
-            //        Item = Serialize(entry),
-            //        Expiration = utcExpiry
-            //    });
-            //}
+            if (item != null)
+            {
+                item.Item = Serialize(entry);
+                item.Expiration = utcExpiry;
+                _cacheItems.Save(item);
+            }
+            else
+            {
+                _cacheItems.Insert(new CacheItem
+                {
+                    Id = key,
+                    Item = Serialize(entry),
+                    Expiration = utcExpiry
+                });
+            }
         }
 
+        /// <summary>
+        /// 重写Remove方法
+        /// </summary>
+        /// <param name="key"></param>
         public override void Remove(string key)
         {
             Debug.WriteLine("Cache.Remove({0})", key);
 
             key = MD5(key);
-
-            //_cacheItems.Remove(new { _id = key });    //Remove方法有问题
+            var query = Query<CacheItem>.EQ(e => e.Id, key);
+            _cacheItems.Remove(query);    //Remove方法有问题
         }
 
         private static string MD5(string value)
@@ -150,8 +173,15 @@ namespace MVCExtend.CustomeOutputProvider
 
         public void Dispose()
         {
-            _mongo.Disconnect();
-            //_mongo.Dispose();
+            if (_mongoserver != null)
+            {
+                _mongoserver = null;
+            }
+            if (_mongoclient != null)
+            {
+                _mongoclient = null;
+            }
+            //_mongoserver.RequestDone();
         }
     }
 }
