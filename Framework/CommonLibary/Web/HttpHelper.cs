bebac059706fd8836e6ea623a64a8314c8e7d6fa
codezyc <zyc199144@gmail.com>
2014-03-15 17:08:17 +0800

    提交HttpHelper代码

---
+++ Framework/CommonLibary/Web/HttpHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CommonLibary.Web
+{
+    /// <summary>
+    /// Http常用方法操作
+    /// </summary>
+    public class HttpHelper
+    {
+        /// <summary>
+        /// 键值对的QueryString集合转换成Url后面的参数字符串
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static async Task<string> SetUrlQueryString(Dictionary<string, string> data)
+        {
+            if (data != null)
+            {
+                var querystring = new FormUrlEncodedContent(data);
+                return await querystring.ReadAsStringAsync();
+            }
+            return null;
+        }
+    }
+}
