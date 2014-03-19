b83e1990b77e0cbb4e11352b4e83019d4f9a934f
codezyc <zyc199144@gmail.com>
2014-03-19 19:15:13 +0800

    提交今日代码

---
+++ Framework/MVCExtend/CustomeAttribute/LayoutAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+
+namespace MVCExtend.CustomeAttribute
+{
+    public class LayoutAttribute : ActionFilterAttribute
+    {
+        /// <summary>
+        /// 模板页的名称
+        /// </summary>
+        private readonly string _masterName;
+        public LayoutAttribute(string masterName)
+        {
+            _masterName = masterName;
+        }
+
+        /// <summary>
+        /// 重写OnActionExecuted方法
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+            var result = filterContext.Result as ViewResult;
+            if (result != null)
+            {
+                result.MasterName = _masterName;
+            }
+        }
+    }
+}
