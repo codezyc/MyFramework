8f375cfa0393cce7f46ebed6ad3a5d15305697ad
codezyc <zyc199144@gmail.com>
2014-03-28 13:40:50 +0800

    添加表达式的扩展类

---
+++ Framework/CommonLibary/ExpressionExt/ParameterRebinder.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace CommonLibary.ExpressionExt
+{
+    /// <summary>
+    /// 表达式参数重新绑定
+    /// </summary>
+    public class ParameterRebinder : ExpressionVisitor
+    {
+        private readonly Dictionary<ParameterExpression, ParameterExpression> map;
+
+        public ParameterRebinder(Dictionary<ParameterExpression, ParameterExpression> map)
+        {
+            this.map = map ?? new Dictionary<ParameterExpression, ParameterExpression>();
+        }
+
+        public static Expression ReplaceParameters(Dictionary<ParameterExpression, ParameterExpression> map, Expression exp)
+        {
+            return new ParameterRebinder(map).Visit(exp);
+        }
+
+        /// <summary>
+        /// 重写VisitParameter方法
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        protected override Expression VisitParameter(ParameterExpression p)
+        {
+            ParameterExpression replacement;
+            if (map.TryGetValue(p, out replacement))
+            {
+                p = replacement;
+            }
+            return base.VisitParameter(p);
+        }
+    }
+}
