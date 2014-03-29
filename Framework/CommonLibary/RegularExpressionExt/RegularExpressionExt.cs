6411732b163156bb02f76a155d2997c30d638360
codezyc <zyc199144@gmail.com>
2014-03-29 09:24:26 +0800

    添加正则表达式的扩展方法类
    继续努力

---
+++ Framework/CommonLibary/RegularExpressionExt/RegularExpressionExt.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace CommonLibary.RegularExpressionExt
+{
+    /// <summary>
+    /// 正则表达式常用扩展方法
+    /// </summary>
+    public static class RegularExpressionExt
+    {
+        /// <summary>
+        /// Returns the input typed as a generic IEnumerable of the groups
+        /// </summary>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        public static IEnumerable<System.Text.RegularExpressions.Group> AsEnumerable(
+            this System.Text.RegularExpressions.GroupCollection gc)
+        {
+            foreach (System.Text.RegularExpressions.Group g in gc)
+            {
+                yield return g;
+            }
+        }
+
+        /// <summary>
+        /// Returns the input typed as a generic IEnumerable of the matches
+        /// </summary>
+        /// <param name="mc"></param>
+        /// <returns></returns>
+        public static IEnumerable<System.Text.RegularExpressions.Match> AsEnumerable(this System.Text.RegularExpressions.MatchCollection mc)
+        {
+            foreach (System.Text.RegularExpressions.Match m in mc)
+            {
+                yield return m;
+            }
+        }
+    }
+}
