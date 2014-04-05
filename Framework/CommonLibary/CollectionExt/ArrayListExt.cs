63f1c9834c8375846a3ca1f53f8f8872d539d38e
codezyc <zyc199144@gmail.com>
2014-04-05 20:30:47 +0800

    添加扩展方法

---
+++ Framework/CommonLibary/CollectionExt/ArrayListExt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibary.CollectionExt
+{
+    /// <summary>
+    /// ArrayList的常用扩展方法
+    /// </summary>
+    public static class ArrayListExt
+    {
+        /// <summary>
+        /// Convert ArrayList To List<T>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="arraylist"></param>
+        /// <returns></returns>
+        public static List<T> ToList<T>(this ArrayList arraylist)
+        {
+            List<T> list = new List<T>();
+            foreach (var item in arraylist)
+            {
+                list.Add((T)item);
+            }
+            return list;
+        }
+    }
+}
