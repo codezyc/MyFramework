a61020856c1f700622e55ce89d55d0565b7cb73c
codezyc <zyc199144@gmail.com>
2014-04-07 19:16:57 +0800

    commit

--- Framework/WebAPIExtend/App_Start/WebApiConfig.cs
+++ Framework/WebAPIExtend/App_Start/WebApiConfig.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Controllers;
 using System.Web.Http.ExceptionHandling;
 using System.Web.Routing;
 using WebAPIExtend.CustomeHandler;
+using WebAPIExtend.CustomeParameterBinding;
 
 namespace WebAPIExtend
 {
@@ -24,6 +26,9 @@ namespace WebAPIExtend
                 defaults: new { id = RouteParameter.Optional }
             );
 
+            // Register an action to create custom ParameterBinding
+            config.ParameterBindingRules.Insert(0, GetCustomParameterBinding);
+
             config.Services.Add(typeof(IExceptionLogger), new TraceSourceExceptionLogger(new TraceSource("MyTraceSource", SourceLevels.All)));
 
             config.Services.Replace(typeof(IExceptionHandler), new GlobalExceptionHandler());
@@ -35,6 +40,15 @@ namespace WebAPIExtend
                 defaults: new { id = RouteParameter.Optional }
             ).RouteHandler = new SessionRouteHandler();
             #endregion
+        }
+
+        public static HttpParameterBinding GetCustomParameterBinding(HttpParameterDescriptor descriptor)
+        {
+            if (descriptor.ParameterType == typeof(object))
+            {
+                return new FromUriOrBodyParameterBinding(descriptor);
+            }
+            return null;
         }
     }
 }
